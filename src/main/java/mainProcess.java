4ba3f18fdc340a85dda65abaa06fffd653c499c0
Hilosh <18868702264@163.com>
2019-07-10 10:32:09 +0800

    试一下jdk8的新特性

--- src/main/java/mainProcess.java
+++ src/main/java/mainProcess.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -8,9 +10,12 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-
+@FunctionalInterface
+interface TryOnce {
+    String MD5(String query,String location,String radius,String output,String ak,String filter,String scope
+            ,String page_num,String sk) throws UnsupportedEncodingException;
+}
 public class mainProcess {
-
 
     private static boolean isdone(List<FutureTask<String>> list) throws ExecutionException, InterruptedException {//所有线程是否完成
         for(FutureTask<String> futureTask:list){
@@ -20,7 +25,42 @@ public class mainProcess {
     }
 
     public static void main(String[] args){
+        TryOnce tryOnce = (query, location, radius, output, ak, filter, scope, page_num, sk) -> {
+            Map<String,String> paramsMap = new LinkedHashMap<>();
+            //map赋值
+            paramsMap.put("query",query);
+            paramsMap.put("location",location);
+            paramsMap.put("radius",radius);
+            paramsMap.put("output",output);
+            paramsMap.put("ak",ak);
+            paramsMap.put("filter",filter);
+            paramsMap.put("scope",scope);
+            paramsMap.put("page_size","1");
+            paramsMap.put("page_num",page_num);
+
+            //编码拼接
+//        StringBuffer queryString = new StringBuffer();
+//        for(Map.Entry<String,String> pair:paramsMap.entrySet()){
+//            queryString.append(pair.getKey()+"=");
+//            queryString.append(URLEncoder.encode((String)pair.getValue(),"UTF-8")+"&");
+//        }
+//        if(queryString.length()>1)queryString.deleteCharAt(queryString.length()-1);
+//        String wholeString = new String("/place/v2/search?"+queryString.toString()+sk);
+//        wholeString = URLEncoder.encode(wholeString,"UTF-8");
+//        String sn = MD5_sn(wholeString);
+            StringBuilder resString = new StringBuilder();
+            for(Map.Entry<String,String> pair:paramsMap.entrySet()){
+                resString.append(pair.getKey()).append("=");
+                resString.append(pair.getValue()).append("&");
+            }
+            if(resString.length()>1)resString.deleteCharAt(resString.length()-1);
+            return "/place/v2/search?"+resString;
+        };
         try {
+            Executor pool = new ThreadPoolExecutor(5, 10,
+                    180L,
+                    TimeUnit.SECONDS,
+                    new LinkedBlockingDeque<>());
             File file = new File("src/main/resources/res.xls");
             //获取输入流
             InputStream stream = new FileInputStream(file);
@@ -29,7 +69,7 @@ public class mainProcess {
             Row row = sheet.getRow(0);
             simpleThread.setSheet(sheet);
             ExecutorService executorService =Executors.newFixedThreadPool(4);
-            List<FutureTask<String>> futureTaskList = new ArrayList<FutureTask<String>>();//希望等到所有子线程结束，主线程才继续
+            List<FutureTask<String>> futureTaskList = new ArrayList<>();//希望等到所有子线程结束，主线程才继续
             Long start = System.currentTimeMillis();
             for(int i =0;i<3;i++){
                 futureTaskList.add(new FutureTask<String>(new simpleThread("地铁站","100000","json","------"
@@ -48,15 +88,8 @@ public class mainProcess {
                 xssfWorkbook.close();
                 outputStream.close();
                 stream.close();
-                return;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (IOException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
