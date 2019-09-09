e7d5d652a115f36e055c34177fa821a32669abf9
jyuh <jyuh@servyou.com.cn>
2019-09-09 19:23:57 +0800

    修改

--- src/main/java/mainProcess.java
+++ src/main/java/mainProcess.java
@@ -10,11 +10,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-@FunctionalInterface
-interface TryOnce {
-    String MD5(String query,String location,String radius,String output,String ak,String filter,String scope
-            ,String page_num,String sk) throws UnsupportedEncodingException;
-}
 public class mainProcess {
 
     private static boolean isdone(List<FutureTask<String>> list) throws ExecutionException, InterruptedException {//所有线程是否完成
@@ -25,42 +20,7 @@ public class mainProcess {
     }
 
     public static void main(String[] args){
-        TryOnce tryOnce = (query, location, radius, output, ak, filter, scope, page_num, sk) -> {
-            Map<String,String> paramsMap = new LinkedHashMap<>();
-            //map赋值
-            paramsMap.put("query",query);
-            paramsMap.put("location",location);
-            paramsMap.put("radius",radius);
-            paramsMap.put("output",output);
-            paramsMap.put("ak",ak);
-            paramsMap.put("filter",filter);
-            paramsMap.put("scope",scope);
-            paramsMap.put("page_size","1");
-            paramsMap.put("page_num",page_num);
-
-            //编码拼接
-//        StringBuffer queryString = new StringBuffer();
-//        for(Map.Entry<String,String> pair:paramsMap.entrySet()){
-//            queryString.append(pair.getKey()+"=");
-//            queryString.append(URLEncoder.encode((String)pair.getValue(),"UTF-8")+"&");
-//        }
-//        if(queryString.length()>1)queryString.deleteCharAt(queryString.length()-1);
-//        String wholeString = new String("/place/v2/search?"+queryString.toString()+sk);
-//        wholeString = URLEncoder.encode(wholeString,"UTF-8");
-//        String sn = MD5_sn(wholeString);
-            StringBuilder resString = new StringBuilder();
-            for(Map.Entry<String,String> pair:paramsMap.entrySet()){
-                resString.append(pair.getKey()).append("=");
-                resString.append(pair.getValue()).append("&");
-            }
-            if(resString.length()>1)resString.deleteCharAt(resString.length()-1);
-            return "/place/v2/search?"+resString;
-        };
         try {
-            Executor pool = new ThreadPoolExecutor(5, 10,
-                    180L,
-                    TimeUnit.SECONDS,
-                    new LinkedBlockingDeque<>());
             File file = new File("src/main/resources/res.xls");
             //获取输入流
             InputStream stream = new FileInputStream(file);
@@ -72,7 +32,7 @@ public class mainProcess {
             List<FutureTask<String>> futureTaskList = new ArrayList<>();//希望等到所有子线程结束，主线程才继续
             Long start = System.currentTimeMillis();
             for(int i =0;i<3;i++){
-                futureTaskList.add(new FutureTask<String>(new simpleThread("地铁站","100000","json","------"
+                futureTaskList.add(new FutureTask<>(new simpleThread("地铁站","100000","json","--------"
                         ,"sort_name:distance|sort_rule:1","2","0","*****")));
                 executorService.submit(futureTaskList.get(i));
             }
